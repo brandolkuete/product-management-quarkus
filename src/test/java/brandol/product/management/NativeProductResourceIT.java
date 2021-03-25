487a0f6001d5f78a6128d7e609594ed370477c6e
brandolkuete <brandolkuete95@gmail.com>
2021-03-25 14:12:01 +0100

    First commit

---
+++ src/test/java/brandol/product/management/NativeProductResourceIT.java
@@ -0,0 +1,9 @@
+package brandol.product.management;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeProductResourceIT extends ProductResourceTest {
+
+    // Execute the same tests but in native mode.
+}
